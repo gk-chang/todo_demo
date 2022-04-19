b9ff1fb66152ff654166587b493e74a46592627a
gk-chang <zhikai_chang@163.com>
2022-04-19 11:06:42 +0800

    npm run build 第一次构建完成发布版

---
+++ demo/src/App.vue
@@ -0,0 +1,71 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <main>
+    <div class="container">
+      <h1>欢迎使用</h1>
+      <todo-add></todo-add>
+      <todo-filer></todo-filer>
+      <todo-list></todo-list>
+    </div>
+  </main>
+</template>
+
+<script>
+import TodoAdd from './components/TodoAdd.vue'
+import TodoFiler from './components/TodoFiler.vue'
+import TodoList from './components/TodoList.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TodoAdd,
+    TodoFiler,
+    TodoList
+    // HelloWorld
+  }
+}
+</script>
+
+<style>
+* {
+  box-sizing:border-box;
+  margin:0;
+  padding:0;
+  font-family: Helvetica, "gengsha", "Microsoft Yahei", sans-serif
+}
+
+img {
+  width:50;
+  display: none;
+}
+
+/*整个页面*/
+main
+{
+  width: 100vw;
+  min-height: 100vh;
+  display: grid;
+  align-items: center;
+  justify-items: center;
+  background: rgb(203, 210, 240);
+  }
+
+.container {
+  width: 60%;
+  max-width: 400px;
+  box-shadow: 0px 0px 24px rgba(0, 0, 0, 0.15);
+  border-radius: 24px;
+  padding: 48px 28px;
+  background-color: rgb(245, 246, 252);
+}
+
+h1 {
+  margin:24px 0;
+  font-size: 28px;
+  color: #414873;
+}
+
+
+</style>
---
+++ demo/src/components/TodoList.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="todo-list">
+        <todo-list-item v-for="n in 3" :key="n"></todo-list-item>
+    </div>
+</template>
+
+<script>
+import TodoListItem from './TodoListItem';
+export default {
+    components: { TodoListItem },
+    name:"TodoList",
+
+}
+</script>
+
+<style>
+.todo-list {
+  display:grid;
+  row-gap:14px;
+}
+
+</style>
